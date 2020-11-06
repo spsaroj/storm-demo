ee2ff16bfc4d9de1dda11e91330b881295fda320
Tiwari <S527999@nwmissouri.edu>
2020-11-06 13:24:31 -0600

    Updated the readspout and topology file

--- src/main/java/edu/nwmissouri/bigdatastorm/Topology.java
+++ src/main/java/edu/nwmissouri/bigdatastorm/Topology.java
@@ -1,2 +1,26 @@
 package edu.nwmissouri.bigdatastorm;
 
+public class Topology{
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new ReadSpout(), 5);
+        builder.setBolt("split", new SplitBolt(), 8).shuffleGrouping("spout");
+        builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(3);
+
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        } 
+        else {
+        conf.setMaxTaskParallelism(3);
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("word-count", conf, builder.createTopology());
+        Thread.sleep(10000);
+        cluster.shutdown();
+        }
+    }
+}
